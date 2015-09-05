2219dea981c45ac66230f7e36d1a7b76525c9a93
ShiCang <shicang1990@gmail.com>
2015-09-05 23:16:11 +0800

    Initial Commit

---
+++ SCMacros/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCMacros
+//
+//  Created by ShiCang on 15/9/5.
+//  Copyright (c) 2015年 SCLibrary. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
